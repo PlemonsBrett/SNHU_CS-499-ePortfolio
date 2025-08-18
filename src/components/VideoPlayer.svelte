85e919d7dd472e3a5a3223bd775b5834c1ce68bd
PlemonsBrett <Brett.Plemons@gmail.com>
2025-08-17 21:31:11 -0500

    Add video player with Code Review video hosted on CloudFlare

---
+++ src/components/VideoPlayer.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let src;
+  export let width = "100%";
+  export let maxWidth = "800px";
+</script>
+
+<video
+  controls
+  preload="metadata"
+  {width}
+  style="max-width: {maxWidth}; border-radius: 8px;"
+>
+  <source {src} type="video/mp4" />
+  Your browser doesn't support the video tag
+</video>
